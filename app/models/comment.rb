72562245c89321163024f0df7a3c1bb6d8740cd5
kazutokato <e.t.s.w.a.n.a@gmail.com>
2021-02-07 11:58:58 +0000

    layout

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :end_user
   belongs_to :review
 
-  validates :body, presence: true, length: { maximum: 60 }
+  validates :body, presence: true, length: { maximum: 40 }
 
 end
