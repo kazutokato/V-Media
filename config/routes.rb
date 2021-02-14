b8f3344a1f675e6b6f2633ac870952b0b2b86914
kazutokato <e.t.s.w.a.n.a@gmail.com>
2021-02-14 08:36:29 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
     sessions: 'admins/sessions'
   }
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
