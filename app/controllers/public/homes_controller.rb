d6dcbf8771ad1d664aa9ccf11e69981ef834e2d9
kazutokato <e.t.s.w.a.n.a@gmail.com>
2021-02-17 04:35:14 +0000

    [Update] layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
-  def top
-  end
 
-  def about
+  def top
+    # いいね数順に第三位までのレビューのIDを取得する
+    @all_ranks = Review.find(Favorite.group(:review_id).order('count(review_id) desc').limit(3).pluck(:review_id))
   end
 
 end
